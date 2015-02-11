6479ca14070fe065b0fed8c001101224c3a26701
josebigio <sirownsalot@gmail.com>
2015-02-11 17:36:38 -0600

    created a simple solution

--- Netflix.py
+++ Netflix.py
@@ -1,5 +1,34 @@
+#!/usr/bin/env python3
+
 import json 
+import fileinput
+import math
+import string
 
-cache = json.load(open('pma459-mvAvgCache.json', 'r'))
+movieRatingCache = json.load(open('pma459-mvAvgCache.json', 'r'))
+answersCache = json.load(open('pma459-answersCache.json', 'r'))
 
-print cache
+
+#read in a line
+movieID = ""
+sqrtSum = 0
+count = 0
+for line in fileinput.input():
+	if ":" in line:
+		#we have a new movie
+		movieID = line.split(":")[0]
+		print(movieID + ":")
+	else:
+		#we are predicting the rating userID gave to movieID
+		userID = line.rstrip()
+		predictedRating = float(movieRatingCache[int(movieID)])
+		print(round(predictedRating,2))
+		realRating = float(answersCache[movieID][userID])
+		diff = predictedRating - realRating
+		sqrtSum += diff **2
+		count+=1
+sqrtError = round(((sqrtSum)/count)**0.5,2)
+print ("RMSE: " + str(sqrtError))
+
+
+
